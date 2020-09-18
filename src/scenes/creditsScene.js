3035ed062d73976a043fa07b6ddfc6df67297621
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-09-18 00:32:23 -0500

    Add credits placeholder.

--- src/scenes/creditsScene.js
+++ src/scenes/creditsScene.js
@@ -1,13 +1,54 @@
-import 'phaser';
- 
+import "phaser";
+import config from "../config/config";
+
 export default class CreditsScene extends Phaser.Scene {
-  constructor () {
-    super('Credits');
+  constructor() {
+    super("Credits");
   }
- 
-  preload () {
+
+  create() {
+    this.creditsText = this.add.text(0, 0, "Credits", {
+      fontSize: "32px",
+      fill: "#fff",
+    });
+    this.madeByText = this.add.text(0, 0, "Created By: Oscar Alfredo Gómez Elizarrarás", {
+      fontSize: "26px",
+      fill: "#fff",
+    });
+    this.zone = this.add.zone(
+      config.width / 2,
+      config.height / 2,
+      config.width,
+      config.height
+    );
+
+    Phaser.Display.Align.In.Center(this.madeByText, this.zone);
+
+    Phaser.Display.Align.In.Center(this.creditsText, this.zone);
+
+    this.madeByText.setY(600);
+
+    this.creditsTween = this.tweens.add({
+      targets: this.creditsText,
+      y: -100,
+      ease: "Power1",
+      duration: 3000,
+      delay: 1000,
+      onComplete: function () {
+        this.destroy;
+      },
+    });
+
+    this.madeByTween = this.tweens.add({
+      targets: this.madeByText,
+      y: -300,
+      ease: "Power1",
+      duration: 8000,
+      delay: 1000,
+      onComplete: function () {
+        this.madeByTween.destroy;
+        this.scene.start("Title");
+      }.bind(this),
+    });
   }
- 
-  create () {
-  }
-};
+}
