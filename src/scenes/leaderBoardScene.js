4872010f488dae56cafe201ad5ad2dce7546fb27
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-10-03 14:26:49 -0500

    Chenge the way the scores were displayed.

--- src/scenes/leaderBoardScene.js
+++ src/scenes/leaderBoardScene.js
@@ -3,6 +3,7 @@
 /* eslint-disable no-unused-expressions */
 import 'phaser';
 import config from '../config/config';
+import UiButton from '../objects/uiButton';
 import { getScores } from '../objects/data';
 
 export default class LeaderBoard extends Phaser.Scene {
@@ -12,12 +13,8 @@ export default class LeaderBoard extends Phaser.Scene {
 
   create() {
     this.scores = '';
+    this.menuButton = new UiButton(this, 400, 500, 'redButton', 'greenButton', 'Menu', 'Title');
     getScores().then((scores) => {
-      this.creditsText = this.add.text(0, 0, 'Scores', {
-        fontSize: '32px',
-        fill: '#fff',
-      });
-
       this.zone = this.add.zone(
         config.width / 2,
         config.height / 2,
@@ -25,39 +22,15 @@ export default class LeaderBoard extends Phaser.Scene {
         config.height,
       );
 
-      Phaser.Display.Align.In.Center(this.creditsText, this.zone);
       for (let i = 0; i < scores.result.length; i += 1) {
-        this.scores += `PLAYER: ${scores.result[i].user}, SCORE: ${scores.result[i].score}\n\n\n`;
+        this.scores += `PLAYER: ${scores.result[i].user}, SCORE: ${scores.result[i].score}\n`;
       }
       this.madeByText = this.add.text(0, 0, this.scores, {
-        fontSize: '26px',
+        fontSize: '12px',
         fill: '#fff',
       });
 
       Phaser.Display.Align.In.Center(this.madeByText, this.zone);
-
-      this.madeByText.setY(config.height + 100);
-
-      this.creditsTween = this.tweens.add({
-        targets: this.creditsText,
-        y: -100,
-        ease: 'Power1',
-        duration: 3000,
-        delay: 1000,
-        onComplete: this.destroy,
-      });
-
-      this.madeByTween = this.tweens.add({
-        targets: this.madeByText,
-        y: config.height * -1,
-        ease: 'Power1',
-        duration: 40000,
-        delay: 1,
-        onComplete: function () {
-          this.madeByTween.destroy;
-          this.scene.start('Title');
-        }.bind(this),
-      });
     });
   }
 }
