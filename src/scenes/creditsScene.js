47056b001be4385d2195a53f89cab591c7ced09c
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-10-05 16:29:23 -0500

    Add more acknowledgments to credits scene.

--- src/scenes/creditsScene.js
+++ src/scenes/creditsScene.js
@@ -9,14 +9,25 @@ export default class CreditsScene extends Phaser.Scene {
     super('Credits');
   }
 
+  preload () {
+    this.credits =  'Created By: \nOscar Alfredo Gómez Elizarrarás\n\n\n';
+    this.credits += 'Game requirements by: \nMicroverse\n\n\n';
+    this.credits += 'Adventure Girl by: \npzUH on OpenGameArt\n\n\n';
+    this.credits += 'Desert background and tile by: \npzUH\n\n\n';
+    this.credits += 'Sound by: \nCleyton Kauffman\n\n\n';
+    this.credits += 'Game template by: \nZenva tutorial\n\n\n';
+    this.credits += 'Created with: \nPhaser 3';
+  }
+
   create() {
     this.creditsText = this.add.text(0, 0, 'Credits', {
       fontSize: '32px',
       fill: '#fff',
     });
-    this.madeByText = this.add.text(0, 0, 'Created By: Oscar Alfredo Gómez Elizarrarás', {
+    this.madeByText = this.add.text(0, 0, this.credits, {
       fontSize: '26px',
       fill: '#fff',
+      align: 'center',
     });
     this.zone = this.add.zone(
       config.width / 2,
@@ -42,9 +53,9 @@ export default class CreditsScene extends Phaser.Scene {
 
     this.madeByTween = this.tweens.add({
       targets: this.madeByText,
-      y: -300,
+      y: -1200,
       ease: 'Power1',
-      duration: 8000,
+      duration: 23000,
       delay: 1000,
       onComplete: function () {
         this.madeByTween.destroy;
